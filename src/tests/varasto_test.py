40a9f72c84259a3eb44e25ae2ba4bf9ca912f7a3
hakkanka <anniina.a.hakkinen@student.jyu.fi>
2024-04-14 04:10:46 +0300

    testit ja gitignorea muutettu

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,6 +10,14 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_tilavuus_pienempi_kuin_yksi(self):
+        tyhja = Varasto(-1)
+        self.assertAlmostEqual(tyhja.tilavuus, 0)
+
+    def test_alku_saldo_pienempi_kuin_nolla(self):
+        tyhja = Varasto(10, -2)
+        self.assertAlmostEqual(tyhja.saldo, 0)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
@@ -38,3 +46,28 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_varastoon_laitetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    
+    def test_varastosta_otetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_otetaan_negatiivinen(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara, 0)
+    
+    def test_lisataam_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-8)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_tulostus_oikein(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+    
+

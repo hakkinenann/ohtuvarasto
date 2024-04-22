4c920b6926b9c5d53e5f694618181c3a9ce07198
hakkanka <anniina.a.hakkinen@student.jyu.fi>
2024-04-22 11:02:01 +0300

    virhe korjattu

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita kenttää viela_tilaa tms. asdsdddadasdsa
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
